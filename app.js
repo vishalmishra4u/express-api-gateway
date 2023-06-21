d1b01afca736013b11a600a8e096878bf3366a31
vishalmishra4u <vishal.mishra@climateconnect.digital>
2023-06-22 01:42:26 +0530

    Logger added

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const microservices = require('./microservices');
+const rateLimit = require('express-rate-limit');
+const microservices = require('./microservicesDirectory');
+const logger = require('./logger');
 
 const app = express();
+
+// Apply rate limiting middleware
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 1000, // Limit each IP to 1000 requests per minute
+});
+
+app.use(apiLimiter);
 
 microservices.forEach((service) => {
   const { name, url, routes } = service;
@@ -25,6 +35,12 @@ microservices.forEach((service) => {
       })
     );
   });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
 });
 
 module.exports = app;
